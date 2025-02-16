1e7cf76f7b05b05d5b07fcf3740e8de007bf8e5b
Matheus de Medeiros <dutra.md@hotmail.com>
2025-02-16 13:41:33 -0300

    first app

---
+++ src/main/java/com/agentesaudeonline/agente/online/AgenteOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.agentesaudeonline.agente.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenteOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenteOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agentesaudeonline/agente/online/AgenteOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agentesaudeonline.agente.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenteOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
